4d8f84308ec1bb6e8f2d8466f9b83957b83b1912
mk124 <mk124a@gmail.com>
2014-04-15 03:57:57 +0800

    Fix some mistakes

--- src/main/java/net/gtaun/shoebill/example/vm/dialog/VehicleComponentDialog.java
+++ src/main/java/net/gtaun/shoebill/example/vm/dialog/VehicleComponentDialog.java
@@ -1,37 +1,39 @@
 package net.gtaun.shoebill.example.vm.dialog;
 
+import java.util.Arrays;
+
 import net.gtaun.shoebill.common.dialog.ListDialog;
 import net.gtaun.shoebill.common.dialog.ListDialogItem;
 import net.gtaun.shoebill.constant.VehicleComponentModel;
 import net.gtaun.shoebill.constant.VehicleComponentSlot;
 import net.gtaun.shoebill.object.Player;
 import net.gtaun.shoebill.object.Vehicle;
-import net.gtaun.shoebill.object.VehicleComponent;
 import net.gtaun.util.event.EventManager;
 
 public class VehicleComponentDialog extends ListDialog
 {
-	private VehicleComponent vehicleComponent;
+	// Shoebill 1.0 Style
+	public static ListDialog create(Player player, EventManager rootEventManager, Vehicle veh)
+	{
+		return ListDialog.create(player, rootEventManager)
+			.caption(String.format("Vehicle Component - %s (Id: %d)", veh.getModelName(), veh.getId()))
+			.execute((b) -> Arrays.stream(VehicleComponentSlot.values()).forEach((slot) ->
+				b.item(String.format("Slot %s: %s", slot.name(), VehicleComponentModel.getName(veh.getComponent().get(slot))))))
+			.build();
+	}
 	
 	
+	// Shoebill Milestone 2 Style
 	public VehicleComponentDialog(Player player, EventManager eventManager, Vehicle veh)
 	{
 		super(player, eventManager);
-		this.vehicleComponent = veh.getComponent();
 		setCaption(String.format("Vehicle Component - %s (Id: %d)", veh.getModelName(), veh.getId()));
-	}
-	
-	@Override
-	public void show()
-	{
-		items.clear();
+		
 		for (VehicleComponentSlot slot : VehicleComponentSlot.values())
 		{
-			int componentId = vehicleComponent.get(slot);
+			int componentId = veh.getComponent().get(slot);
 			String item = String.format("Slot %s: %s", slot.name(), VehicleComponentModel.getName(componentId));
 			items.add(new ListDialogItem(item));	
 		}
-		
-		super.show();
 	}
 }
